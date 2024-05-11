e45783403d5a4231ee88d1ea45f4726141a1dd98
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-05-11 22:40:34 +0200

    Integrated theme functionality into App.js with styled components

--- theme-builder/src/App.js
+++ theme-builder/src/App.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import WebFont from 'webfontloader';
+import { useTheme } from './theme/useTheme';
+import { GlobalStyles } from './theme/GlobalStyles';
+
+
+const Container = styled.div`
+  margin: 5px auto 5px auto;
+`;
 
 function App() {
+  const {theme, themeLoaded, getFonts} = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState(theme);
+
+  useEffect(() => {
+    setSelectedTheme(theme);
+  }, [themeLoaded]);
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: getFonts()
+      }
+    });
+  });
+
   return (
-    <h1>Theme Builder</h1>
+    <>
+    {
+      themeLoaded && <ThemeProvider theme={ selectedTheme }>
+        <GlobalStyles />
+        <Container style={{fontFamily: selectedTheme.font}}>
+          <h1>Theme Builder</h1>
+          <p>
+            This is a theming system with a Theme Switcher and a Theme Builder.
+            Do you want to see the source code? <a href="https://github.com/TeodoraAlina/theme-builder" target="_blank">Click here.</a>
+          </p>
+        </Container>
+      </ThemeProvider>
+    }
+    </>
   );
 }
 
